02510bd5731942c0314e22c1d4ce1c0bea03c9d8
Júlio <jcarsan1000@gmail.com>
2023-09-21 12:18:48 -0300

    Correção da URI de conexão com o bando de dados

--- src/models/db/db.ts
+++ src/models/db/db.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
-//const dbURI = process.env.MONGO_DB || "";
-const dbURI = "mongodb+srv://jcarsan1000:v4kqWNWBetOw7Coq@livrariavirtual.i4ovnl7.mongodb.net/?retryWrites=true&w=majority";
+const dbURI = process.env.MONGO_DB || "";
 
 mongoose.connect(dbURI);
 
